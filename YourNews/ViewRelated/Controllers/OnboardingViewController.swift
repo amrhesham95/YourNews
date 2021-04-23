586ac09f2ff0dd1a3dc59130291118a73e967f4c
Amr Hesham <amrhesham9595@gmail.com>
2021-04-23 15:33:52 +0200

    - Adding basic categories row - Configuring submit button

--- YourNews/ViewRelated/Controllers/OnboardingViewController.swift
+++ YourNews/ViewRelated/Controllers/OnboardingViewController.swift
@@ -14,6 +14,7 @@ class OnboardingViewController: FormViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    configureFooterView()
     createRows()
   }
   
@@ -22,14 +23,28 @@ class OnboardingViewController: FormViewController {
 // MARK: - Rows Creations
 //
 extension OnboardingViewController {
+  
+  /// Creating inputs rows of OnboardingViewController
+  ///
   func createRows() {
-//    configureFooterView()
     form +++ Section()
       <<< PushRow<String>() { row in
         row.title = "Countries"
         row.options = ["Egypt", "KSA", "UAE"]
       }
+    
+      <<< MultipleSelectorRow<String>() { row in
+        row.title = "Categories"
+        row.options = ["Category1", "Category2", "Category3"]
+      }
   }
   
+  /// Configure footer view
+  ///
+  func configureFooterView() {
+    footerView.addAction(ButtonAction(title: "Submit", style: .default) { [weak self] _ in
+      print("Submit button was tapped")
+    })
+  }
 }
 
