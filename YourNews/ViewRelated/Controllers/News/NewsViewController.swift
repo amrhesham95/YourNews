a2925a821a9e221d3b60ffe2919340f3d93e785d
Amr Hesham <amrhesham9595@gmail.com>
2021-04-24 13:26:17 +0200

    - indenting code - add newsSearchCommand instance

--- YourNews/ViewRelated/Controllers/News/NewsViewController.swift
+++ YourNews/ViewRelated/Controllers/News/NewsViewController.swift
@@ -10,66 +10,72 @@ import Foundation
 import UIKit
 
 class NewsViewController: UIViewController {
-
- 
-    // MARK: - Outlets
+  
+  
+  // MARK: - Outlets
+  
+  @IBOutlet weak var contentView: UIView!
+  
+  // MARK: - Properties
+  
+  /// MyTicketsListSelectorDataSource
+  ///
+  private(set) lazy var newsSearchCommand: NewsSearchCommand = {
+    return NewsSearchCommand(onSelectSearchResult: handleOnSelectArticle(article:))
+  }()
+  
+  /// Search view controller for all departments
+  ///
+  lazy var searchViewController: SearchViewController = {
+    return SearchViewController(
+      command: newsSearchCommand,
+      cellType: BasicTableViewCell.self,
+      cellSeparator: .singleLine
+    )
+  }()
+  
+  
+  
+  // MARK: - ViewLifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var contentView: UIView!
-    
-    // MARK: - Proeprties
-    
-    /// Search view controller for all departments
-    ///
-    lazy var searchViewController: SearchViewController = {
-      return SearchViewController(
-        command: NewsSearchCommand(onSelectSearchResult: handleOnSelectArticle(article:)),
-        cellType: BasicTableViewCell.self,
-        cellSeparator: .singleLine
-      )
-    }()
-    
-
-    
-    // MARK: - ViewLifecycle
-    
-    override func viewDidLoad() {
-      super.viewDidLoad()
-      
-      configureListViewController()
-    }
+    configureListViewController()
   }
-
-  // MARK: - View's Configuration
-  //
-  private extension NewsViewController {
-    
-    /// Apply configuration for list view controller
-    ///
-    func configureListViewController() {
-      addChildViewController(searchViewController,
-                             parentView: contentView)
-    }
-  }
-
-  // MARK: - Private handlers
-  //
-  private extension NewsViewController {
-    
-    func handleOnSelectArticle(article: News) {
-      print(article)
-    }
 }
 
- //MARK: - UITableViewCell+SearchResultCell
+// MARK: - View's Configuration
+//
+private extension NewsViewController {
+  
+  /// Apply configuration for list view controller
+  ///
+  func configureListViewController() {
+    addChildViewController(searchViewController,
+                           parentView: view)
+  }
+}
+
+// MARK: - Private handlers
+//
+private extension NewsViewController {
+  
+  func handleOnSelectArticle(article: News) {
+    print(article)
+  }
+}
+
+//MARK: - UITableViewCell+SearchResultCell
 
 extension BasicTableViewCell: SearchResultCell {
-
+  
   typealias SearchModel = News
-
+  
   static func register(for tableView: UITableView) {
     tableView.register(self, forCellReuseIdentifier: self.classNameWithoutNamespaces)
   }
-
+  
   func configureCell(searchModel: SearchModel) {
     textLabel?.text = searchModel.title
     detailTextLabel?.text = searchModel.author
