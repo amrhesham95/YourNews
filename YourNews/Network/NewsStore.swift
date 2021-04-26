b129083ed2bb90340ecdddf88f19b70b27c0a5ff
Amr Hesham <amrhesham9595@gmail.com>
2021-04-26 22:07:50 +0200

    Preparing for caching

--- YourNews/Network/NewsStore.swift
+++ YourNews/Network/NewsStore.swift
@@ -14,7 +14,7 @@ class NewsStore: NewsStoreProtocol {
   // MARK: - Properties
   
   var remote: NewsRemoteProtocol?
-  
+  let storageManager = RealmStorage()
   // MARK: - Init
   
   init(remote: NewsRemoteProtocol) {
@@ -35,6 +35,46 @@ class NewsStore: NewsStoreProtocol {
   }
 }
 
+// MARK: News Handlers
+
+// TODO: TO BE USED WHEN START CACHING and PAGINATION WITH REALM
+extension NewsStore {
+  
+  func getAllNews(request: NewsSearchRequest, searchWord: String, complitionHandler: @escaping ([StorageNews]?, Error?) -> Void) {
+    
+    let isNetworkAvailable = Reachability.isConnectedToNetwork()
+    isNetworkAvailable ? updateNewsThenFetch(request: request, searchWord: searchWord, completion: complitionHandler) : fetchNews(completion: complitionHandler)
+    
+  }
+  
+  func updateNewsThenFetch(request: NewsSearchRequest, searchWord: String, completion: @escaping ([StorageNews]?, Error?) -> Void ) {
+    
+    getHeadlines(request: request, searchWord: searchWord) { [weak self] result in
+      switch result {
+      case .success(let newsResponse):
+        DispatchQueue.main.async {
+          newsResponse.articles?.forEach {
+            try? self?.storageManager.insertNewObject(object: $0.storageNews)
+          }
+          self?.fetchNews(completion: completion)
+        }
+      case .failure(let error):
+        completion(nil, error)
+      }
+    }
+  }
+  
+  func fetchNews(completion: @escaping ([StorageNews]?, Error?) -> Void) {
+    let news = storageManager.allObjects(ofType: StorageNews.self)
+    guard !news.isEmpty else {
+      completion(nil, GeneralError())
+      return
+    }
+    completion(news, nil)
+  }
+}
+
+
 
 
 // MARK: - GeneralError
@@ -49,6 +89,6 @@ extension GeneralError: LocalizedError {
 // MARK: - GeneralError
 //
 public struct GeneralError: Error {
-
+  
   public init() { }
 }
